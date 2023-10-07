08f256309a914ba341349169564e1181085e9499
jonathan-matheus <johhn.dev@gmail.com>
2023-10-07 14:08:17 -0300

    Exibe o conteudo da página

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-require_once('header.php');
-require_once('footer.php');
+<?php require_once('header.php'); ?>
+
+<!-- Exibe o conteudo da págian -->
+<div class="s-48"></div>
+<div class="container">
+    <?php the_content(); ?>
+</div>
+
+<?php require_once('footer.php'); ?>
