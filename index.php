1666cb405e8a55f4bed64c3f819778418c418e38
jonathan-matheus <johhn.dev@gmail.com>
2023-10-13 23:12:05 -0300

    pagina de blog

--- index.php
+++ index.php
@@ -1,24 +1,48 @@
 <?php require_once('header.php'); ?>
 
-<!-- Exibe o conteudo da págian -->
-<div class="s-48"></div>
-<div class="container">
-    <h1 class="font-1-xl w-c"><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-</div>
-
 <?php
-// Verifique se há um post corrente
-if (have_posts()) :
-    while (have_posts()) : the_post();
-        // Use get_the_author() para obter o nome do autor
+// Verifique se há um post corrente, e pegando dados do autor do post
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        // Pega o nome do autor
         $author_name = get_the_author();
-
-        // Agora você pode usar $author_name como o nome do autor
-        echo "O autor deste post é: " . $author_name;
-    endwhile;
-endif;
+        // Pega a data de publicação do post
+        $post_date = get_the_date();
+        // Pega a url do avatar do author
+        $author_avatar_url = get_avatar_url(get_the_author_meta('ID'));
+    }
+}
 ?>
 
+<!-- Exibe o conteudo da págian -->
+<div class="s-48"></div>
+<div class="p2-b">
+    <div class="s-120"></div>
+    <div class="container">
+        <h1 class="font-1-xl w-c"><?php the_title(); ?></h1>
+
+        <?php
+        // Verifica se o post é do tipo post, caso seja, exibe o autor e a data
+        $post_type = get_post_type();
+        if ($post_type == 'post') { ?>
+            <div class="d-flex align-items-center">
+                <div>
+                    <img class="rounded-circle avatar" width="60px" src="<?php echo $author_avatar_url; ?>" alt="avatar do autor">
+                </div>
+                <p class="font-1-m w-c align-items-center">
+                    Autor: <?php echo $author_name; ?><br>
+                    Publicado em: <?php echo $post_date; ?>
+                </p>
+            </div>
+        <?php } ?>
+
+    </div>
+    <div class="s-120"></div>
+</div>
+<div class="s-48"></div>
+<div class="container">
+    <?php the_content(); ?>
+</div>
 
 <?php require_once('footer.php'); ?>
