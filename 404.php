37796629dc2c023774caef0daf3527c8ef4e3603
jonathan-matheus <johhn.dev@gmail.com>
2025-12-25 15:07:40 -0300

    adição da página 404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+get_header();
+?>
+<main class="container mx-auto max-w-4xl entry-content">
+    <article class="mt-[12px]">
+        <h1 class="text-4xl md:text-6xl font-1-xl text-center px-4">404 - Página Não Encontrada</h1>
+        <p class="mt-6 text-center">Desculpe, a página que você está procurando não existe.</p>
+    </article>
+</main>
+<?php
+get_footer();
+?>
