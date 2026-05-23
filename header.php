6b597cbb36dd58cc0ba53fe119b850a495a86b85
jonathan-matheus <johhn.dev@gmail.com>
2026-05-23 17:16:08 -0300

    Atualiza a lógica do título da página para exibir o nome do site na página inicial

--- header.php
+++ header.php
@@ -4,7 +4,14 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title("|", true, "right"); bloginfo("name"); ?></title>
+    <title><?php
+    if (is_home()) {
+        bloginfo('name');
+    } else {
+        echo wp_title('');
+    }
+    ?>
+    </title>
     <?php wp_head(); ?>
 </head>
 
