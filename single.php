0a643711b2c8ef77938eb43f8ced0302c7b14b19
jonathan-matheus <johhn.dev@gmail.com>
2026-05-26 15:30:34 -0300

    Adiciona estilo para o conteúdo de postagens individuais e importa o CSS correspondente

--- single.php
+++ single.php
@@ -27,7 +27,7 @@ get_header();
                     </figure>
                 <?php endif; ?>
 
-                <div class="text-[16px] leading-[28px] font-normal text-[rgba(255,255,255,0.9)] [font-family:-apple-system,BlinkMacSystemFont,'Segoe_UI',Helvetica,Arial,sans-serif,'Apple_Color_Emoji','Segoe_UI_Emoji','Segoe_UI_Symbol'] [&_p]:mb-5 [&_p:last-child]:mb-0 [&_h2]:mt-10 [&_h2]:mb-4 [&_h2]:text-[24px] [&_h2]:leading-[30px] [&_h2]:font-bold [&_h3]:mt-8 [&_h3]:mb-3 [&_h3]:text-[20px] [&_h3]:leading-[28px] [&_h3]:font-bold [&_ul]:mb-5 [&_ul]:list-disc [&_ul]:pl-6 [&_ol]:mb-5 [&_ol]:list-decimal [&_ol]:pl-6 [&_a]:underline">
+                <div class="single-post-content text-[16px] leading-[28px] font-normal text-[rgba(255,255,255,0.9)] [font-family:-apple-system,BlinkMacSystemFont,'Segoe_UI',Helvetica,Arial,sans-serif,'Apple_Color_Emoji','Segoe_UI_Emoji','Segoe_UI_Symbol'] [&_p]:mb-5 [&_p:last-child]:mb-0 [&_h2]:mt-10 [&_h2]:mb-4 [&_h2]:text-[24px] [&_h2]:leading-[30px] [&_h2]:font-bold [&_h3]:mt-8 [&_h3]:mb-3 [&_h3]:text-[20px] [&_h3]:leading-[28px] [&_h3]:font-bold [&_ul]:mb-5 [&_ul]:list-disc [&_ul]:pl-6 [&_ol]:mb-5 [&_ol]:list-decimal [&_ol]:pl-6 [&_a]:underline">
                     <?php the_content(); ?>
                 </div>
             </article>
